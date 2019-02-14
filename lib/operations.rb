9e5eb4d6251eaba0f8191eecfb4eff50a1dddb49
manon jacquin <manon.jacquin@gmail.com>
2019-02-14 14:47:13 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-  speed > 60 ? true : false
+  if (speed > 60) || (speed < 40)
+    true
+  else false
+  end
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 ? true : false
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
